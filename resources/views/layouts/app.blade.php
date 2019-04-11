e138f2e7a654273b7d11b552542becd001dab322
Moeez <resourceinn.com@gmail.com>
2019-04-11 20:31:35 +0500

    login fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+    <script src="{{ asset('js/app.js') }}" ></script>
     {{-- <script  src="{{URL::asset('js/compiler/index.js')}}"></script> --}}
     <script type="text/javascript" src="{{URL::asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
 
